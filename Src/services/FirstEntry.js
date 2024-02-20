e0bf799e522941db45f169223abbb49828bb3772
bmjuzer <juzer.shabir@benchmatrix.com>
2024-02-20 22:48:49 +0500

    J

--- Src/services/FirstEntry.js
+++ Src/services/FirstEntry.js
@@ -1,39 +1,34 @@
 
-const { connectDB, pool } = require("../../config/dbConnection");
+require("dotenv").config(); 
+const { connectDB } = require("../../config/dbConnection");
 
-exports.handler = async (event) => {
+module.exports.handler = async (event) => {
   try {
-    console.log("hello");
+    console.log(process.env.APPLICATION_KEY);
+    console.log("FirstentryJson");
     const FirstentryJson = JSON.parse(event.body);
-
-    await connectDB(); // Establish database connection
-    console.log("Connected to the database");
-
-    console.log("hello1");
-    const result = await new Promise((resolve, reject) => {
-      console.log("hello2");
-      var sql = "INSERT INTO dbo.FirstEntry (title) VALUES ('JS')"; // Replace 'dbo' with the actual schema name
-      console.log("Executing SQL query:", sql);
-      pool.request().query(sql, function (err, result) { // Use pool.request() to execute queries
-        if (err) {
-          console.error("SQL query execution error:", err);
-          return reject(err);
-        }
-        console.log("SQL query executed successfully");
-        resolve(result);
+    console.log(FirstentryJson);
+    const response = await connectDB().then(async (connection) => {
+      return new Promise((resolve, reject) => {
+        var sql = "INSERT INTO Firstentry (title) VALUES ("+FirstentryJson.JS+")";
+        connection.query(sql, function (err, result) {
+          if (err) return reject(err);
+          var res = {
+            statusCode: 200,
+            headers: util.getResponseHeaders(),
+            body: JSON.stringify({ message: "successfully created" }),
+          };
+          resolve(res);
+        });
+        connection.release();
       });
     });
-
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({ message: "successfully created" }),
-    };
-
     return response;
   } catch (err) {
     console.log("Encountered an error:", err);
     return {
       statusCode: err.statusCode ? err.statusCode : 500,
+      headers: util.getResponseHeaders(),
       body: JSON.stringify({
         error: err.name ? err.name : "Exception",
         message: err.message ? err.message : "Unknown error",
