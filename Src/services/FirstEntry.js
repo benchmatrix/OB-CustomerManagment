53db2fc1ab54ab3436bccd99d2ba3a322bd3b9b3
bmjuzer <juzer.shabir@benchmatrix.com>
2024-02-21 13:03:10 +0500

    Paremeter

--- Src/services/FirstEntry.js
+++ Src/services/FirstEntry.js
@@ -1,36 +1,45 @@
 
 require("dotenv").config(); 
-const { connectDB } = require("../../config/dbConnection");
+const { connectDB ,pool} = require("../../config/dbConnection");
 
-module.exports.handler = async (event) => {
+exports.handler = async (event) => {
   try {
-    console.log(process.env.APPLICATION_KEY);
-    console.log("FirstentryJson");
+    console.log("hello");
     const FirstentryJson = JSON.parse(event.body);
-    console.log(FirstentryJson);
-    const response = await connectDB().then(async (connection) => {
-      return new Promise((resolve, reject) => {
-        var sql = "INSERT INTO Firstentry (title) VALUES ("+FirstentryJson.JS+")";
-        connection.query(sql, function (err, result) {
-          if (err) return reject(err);
-          var res = {
-            statusCode: 200,
-            headers: util.getResponseHeaders(),
-            body: JSON.stringify({ message: "successfully created" }),
-          };
-          resolve(res);
-        });
-        connection.release();
+    console.log(process.env.APPLICATION_KEY);
+    await connectDB(); // Establish database connection
+    console.log("Connected to the database");
+
+    console.log("hello1");
+    const result = await new Promise((resolve, reject) => {
+      console.log("hello2");
+    //  var sql = ";
+      console.log("Executing SQL query:", sql);
+     // connection.query('SELECT * FROM health_records where id = ?', [data.id], (err, rows) => {
+        if(err) throw err;
+        response.json({data:rows});
+      pool.request().query("INSERT INTO Firstentry (title,[Add]) VALUES (?,?)",[Firstentry.JS],[Firstentry.add], function (err, result) { // Use pool.request() to execute queries
+        if (err) {
+          console.error("SQL query execution error:", err);
+          return reject(err);
+        }
+        console.log("SQL query executed successfully");
+        resolve(result);
       });
 
       
     });
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ message: "successfully created" }),
+    };
+
     return response;
   } catch (err) {
     console.log("Encountered an error:", err);
     return {
       statusCode: err.statusCode ? err.statusCode : 500,
-      headers: util.getResponseHeaders(),
       body: JSON.stringify({
         error: err.name ? err.name : "Exception",
         message: err.message ? err.message : "Unknown error",
