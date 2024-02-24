bd428cfd63eb666b5ef6086a4f0232a2a42d85ae
bmjuzer <juzer.shabir@benchmatrix.com>
2024-02-24 15:02:42 +0500

    J

--- Src/services/login.js
+++ Src/services/login.js
@@ -1,6 +1,10 @@
-const { CognitoIdentityProviderClient, AdminInitiateAuthCommand } = require("@aws-sdk/client-cognito-identity-provider");
+require("dotenv").config();
+const { CognitoIdentityProviderClient, SignUpCommand,InitiateAuthCommand  } = require("@aws-sdk/client-cognito-identity-provider");
 const jwt = require('jsonwebtoken');
-const { fromIni } = require("@aws-sdk/credential-provider-ini");
+//const { StaticCredentialsProvider } = require("@aws-sdk");
+//const { fromIni } = require("@aws-sdk/credential-provider-ini");
+
+
 exports.handler = async (event) => {
     // Extract user input from the event body
     const { username, password } = JSON.parse(event.body);
@@ -8,7 +12,7 @@ console.log(username);
 console.log(password);
     // Define parameters for the AdminInitiateAuth operation
     const params = {
-        AuthFlow: 'ALLOW_REFRESH_TOKEN_AUTH',
+        AuthFlow: 'USER_PASSWORD_AUTH',
         ClientId: '6qg78b8vprghqonr5tn48pfjjo', // Specify your Cognito app client ID
         UserPoolId: 'me-south-1_KaYL4fupz', // Specify your Cognito user pool ID
         AuthParameters: {
@@ -20,12 +24,30 @@ console.log(password);
     try {
         // Create a new CognitoIdentityProviderClient instance
       //  const client = new CognitoIdentityProviderClient({ region: "me-south-1" });
-        const client = new CognitoIdentityProviderClient({ region: "me-south-1", credentials: fromIni({}) });
+    //  aws_access_key_id = YOUR_ACCESS_KEY_ID
+//aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
+        //const client = new CognitoIdentityProviderClient({ region: "me-south-1", credentials: fromIni({}) });
+        const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region = process.env.AWS_REGION;
 
+// Configuring AWS SDK with credentials and region
+
+        const client = new CognitoIdentityProviderClient({
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey,
+            region: region
+            //,
+          //  credentials: new NewStaticCredentials({
+           //     accessKeyId: 'AKIAXJMDEWVAXYHQVDKY',
+          //      secretAccessKey: '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'
+          //  })
+        });
+        console.log("params")
         console.log("client");
         console.log(params)
         // Call the AdminInitiateAuthCommand to authenticate the user
-        const data = await client.send(new AdminInitiateAuthCommand(params));
+        const data = await client.send(new InitiateAuthCommand(params));
 
         // If authentication is successful, generate JWT token
         if (data && data.AuthenticationResult) {
